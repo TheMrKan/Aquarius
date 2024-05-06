a9e502009a38921a82a8abbd59754bc362ee0270
MrKanYT <mrkan2017@gmail.com>
2024-05-06 20:28:48 +0400

    исправлена загрузка переменной окружения DJANGO_DEBUG

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG")
+DEBUG = bool(int(os.environ.get("DJANGO_DEBUG")))
 
 ALLOWED_HOSTS = ["192.168.137.1", "hd.tlt.ru", "127.0.0.1", "192.168.31.191", "*"]
 
